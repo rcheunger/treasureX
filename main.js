7032350ebbeed46c0903124a473eb9a86c004ec0
rcheunger <rowancheung@gmail.com>
2022-09-27 15:00:32 -0700

    Three JS mockup

---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+    )
+
+const renderer =  new THREE.WebGL1Renderer()
+
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.
+    domElement)
+
+//create sphere
+const sphere = new THREE.Mesh(new THREE.
+    SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({
+        color: 0xFF0000
+    }))
+
+scene.add(sphere)
+
+camera.position.z = 5
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+animate()
